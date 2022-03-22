b2502ebdb025653248dbcb442ed78005eb3664ac
Evgeniy Dmitriev (PE) <evgeniy.dmitriev@astelit.ukr>
2022-03-22 15:24:45 +0200

    Initial commit

---
+++ src/test/java/ua/com/evgeniy/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.evgeniy.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
